a9334e1738b440f5fd67b6b7f2dcf3d58816af9d
alexberry721 <alexberry721@gmail.com>
2016-10-17 09:56:03 -0700

    Bubble sort

--- Code Runner/main.cpp
+++ Code Runner/main.cpp
@@ -7,9 +7,9 @@
 //
 
 
-#define prog 5
+#define prog 2
 
-#if(prog == 5)
+#if(prog == 2)
 
 
 #include <iostream>
@@ -17,274 +17,87 @@ using namespace std;
 
 int main() {
     
-    int i = 1;
+    cout << "Bubble Sort! \n";
     
-    while (i < 10)  {
-        if ( (++i) % 2 == 0)
-            cout << i << endl;
+    // need an integer array
+    const int ARRAY_SIZE = 5;
+    int my_array[ARRAY_SIZE] = {9, 5, 2, 8, 1};
+    
+    
+    cout << "init array state: \n";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << my_array[i] << " ";
     }
     
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#elif(prog == 4)
-
-#include <iostream>
-using namespace std;
-
-int main() {
+    cout << endl;
     
-    int i = 1;
-
-    while (i < 10)  {
-        if ( (++i) % 2 == 0)
-            cout << i << endl;
-    }
-
-    return 0;
-}
-
-#elif(prog == 3)
-
-//  main.cpp
-//  Book Exercise 6.3 - Palindrome Integer
-//
-//  Created by ax on 10/5/16.
-//  Copyright © 2016 COMP130. All rights reserved.
-
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-int return_revese(int& number_input) {
-    
-    int switcherooed = 0;
-    
-    int working_digits = number_input;
-    
-    while (working_digits > 0)
-    {
-        int slice_off_last_digit = working_digits % 10;
+    // need a marker loop
+    // marker sets the bottom point of the iterator loop
+    // counts up (from high to low) by decrement
+    for (int i = ARRAY_SIZE - 1; i >= 0; i--) {
         
-        switcherooed = switcherooed * 10 + slice_off_last_digit;
+        cout << "i: " << i << "\n";
         
-        working_digits = working_digits / 10;
-    }
-    
-    return switcherooed;
-}
-
-bool is_palindrome(int number) {
-    
-    bool palindrome_match = 0;
-    
-    if (return_revese(number) == number)
-        palindrome_match = 1;
-    
-    return palindrome_match;
-}
-
-int main() {
-    
-    int num_input = 0;
-    
-    cout << "Enter some numbers: ";
-    
-    cin >> num_input;
-    
-    cout << endl;
-    
-    if (num_input == 1) {
-        cout << num_input << " is indeed a palidnrome!" << endl;
-    } else {
-        cout << num_input << " is not a palidnrome!" << endl;
-    }
-    
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    
-    return 0;
-    
-}
-
-#elif(prog == 2)
-
-// main.cpp
-// Reverse Numbers
-// Created by ax on 9/28/16.
-// Copyright © 2016 COMP130. All rights reserved.
-
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-bool check_length(string s1, string s2) {
-    
-    bool long_enough = 0;
-    
-    if (!(s1.length() > s2.length()))
-    {
-        long_enough = 1;
-    }
-    
-    return long_enough;
-}
-
-int find_first_position(string s1, string s2) {
-    
-    char first_char_s1 = s1.at(0);
-    int return_val = -1;
-    string check_match_seqence = "";
-    
-    cout << "looking for match to letter \"" << first_char_s1 << "\" in second string." << endl;
-    
-    for (int i = 0; i < s2.length() - s1.length(); i++)
-    {
-        if (first_char_s1 == s2.at(i)) {
-            cout << "Found a first character match at position " << i + 1 << " in the second string." << endl;
-            return_val = i;
+        // need an iterator loop
+        // counts down,
+        // makes compares,
+        // swaps order of numbers
+        for (int j = 0; j <= i - 1; j++) {
+            cout << my_array[j] << "\n";
+            cout << "vs. \n";
+            cout << my_array[j + 1] << "\n";
             
-            // run another function to for loop check the next s1 length of s2
-            
-            cout << "running next checker" << endl;
-            
-            for (int j = 0; j < s1.length(); j++) {
-                cout << "s2.j: " << s2.at(i + j) << endl;
-                check_match_seqence += s2.at(i + j);
+            if (my_array[j] > my_array[j + 1]) {
+                cout << "swap" << "\n";
+                swap(my_array[j], my_array[j + 1]);
             }
-            
         }
         
-    }
-    
-    cout << "string s1: " << s1 << endl;
-    cout << "check match sequence: " << check_match_seqence << endl;
-    
-    cout << "checking if s1 matches check match sequence... " << endl;
-    
-    if (s1 == check_match_seqence) {
-        cout << "we found a match!" << endl;
-    }
-    else {
-        cout << "boo, no match found!" << endl;
-    }
-    
-    
-    return return_val;
-    
-}
-
-int main() {
-    
-    string s1 = " ";
-    string s2 = " ";
-    
-    cout << "Enter string 1: ";
-    
-    getline(cin, s1);
-    
-    cout << "Enter string 2: ";
-    
-    getline(cin, s2);
-    
-    if (check_length(s1, s2) == 0) {
-        cout << "First string is longer than the second string, -1" << endl;
-    }
-    else {
-        cout << "We can work with this! ";
         
-        cout << find_first_position(s1, s2) << endl;
     }
     
+    cout << "end array state: \n";
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << my_array[i] << " ";
+    }
     
+    cout << endl;
+
     
     return 0;
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #elif(prog == 1)
 
-// main.cpp
-// Checksum Calc ISBN13
-// Created by ax on 9/28/16.
-// Copyright © 2016 COMP130. All rights reserved.
-
-#include<iostream>
-#include<string>
-
+#include <iostream>
 using namespace std;
-
-int check_sum(const string& str) {
-    
-    string working = str;
-    int sum_total = 0;
-    
-    for (int i = 0; i < str.length(); i++)
-    {
-        
-        int char_to_int = str.at(i) - 48;
-        
-        if (i % 2 == 0)
-        {
-            sum_total += char_to_int;
-        }
-        else
-        {
-            sum_total += char_to_int * 3;
-        }
-    }
-    
-    return sum_total;
-    
-}
 
 int main() {
     
-    string s1 = " ";
-    
-    cout << "Enter a string of numbers: ";
-    
-    getline(cin, s1);
-    
-    if (s1.length() == 12) {
-        int return_check_sum = check_sum(s1);
-        
-        int d13_calc = 10 - (return_check_sum) % 10;
-        
-        if (d13_calc == 10)
-            d13_calc = 0;
-        
-        char d13 = static_cast<char>(d13_calc + 48);
-        
-        string ISBN13 = s1 += d13;
-        
-        cout << "ISBN13 is: " << ISBN13 << endl;
-    } else {
-        cout << s1 << " is an invalid input" << endl;
-    }
-    
+    int i = 1;
 
-    
+    while (i < 10)  {
+        if ( (++i) % 2 == 0)
+            cout << i << endl;
+    }
+
     return 0;
-    
 }
+
 
 #endif
